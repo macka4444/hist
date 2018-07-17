2b5f608292f0d11c840f0c215d69712458011438
Unknown <macka.baran@gmail.com>
2018-07-17 19:45:58 +1000

    Adds pw reset/change + other refactoring

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import { SignUpLink } from './SignUp';
+import { PasswordForgetLink } from './PasswordForget';
 import { auth } from '../firebase';
 import * as routes from '../constants/routes';
 
-const SignInPage = ({ history }) => <div>
-    <h1>SignIn</h1>
+const SignInPage = ({ history }) =>
+    <div>
+        <h1>SignIn</h1>
         <SignInForm history={history} />
         <SignUpLink />
+        <PasswordForgetLink />
     </div>
 
 const byPropKey = (propertyName, value) => () => ({
