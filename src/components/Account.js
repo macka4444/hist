c5e1f0575f3f3438ea7d63d9c247fd09a1c84949
Unknown <macka.baran@gmail.com>
2018-07-21 08:54:21 +1000

    Added db stuff

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,6 +3,8 @@ import React from 'react';
 import AuthUserContext from './AuthUserContext';
 import { PasswordForgetForm } from './PasswordForget';
 import PasswordChangeForm from './PasswordChange';
+import withAuthorization from './withAuthorization';
+
 
 const AccountPage = () =>
     <AuthUserContext.Consumer>
@@ -15,4 +17,6 @@ const AccountPage = () =>
         }
     </AuthUserContext.Consumer>
 
-export default AccountPage;
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(AccountPage);
